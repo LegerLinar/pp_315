d238a8fbec5f2b8782b705537fadccd01789aeb3
LegerLinar <pavelbesedin97@mail.ru>
2024-07-15 16:38:10 +0700

    Redone

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -27,6 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/api/admin/**").hasRole("ADMIN")
+            .antMatchers("/api/user/**").hasAnyRole("USER", "ADMIN")
             .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
             .anyRequest().authenticated()
             .and()
